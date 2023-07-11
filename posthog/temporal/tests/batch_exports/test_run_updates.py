bfe2edab60efd6874f46ccb5d365385fc621f643
Ryan Hennig <106977715+ryan-pypestream@users.noreply.github.com>
2023-07-11 09:46:23 +0000

    Fix tests in test_run_updates.py by allowing use of fixtures

--- posthog/temporal/tests/batch_exports/test_run_updates.py
+++ posthog/temporal/tests/batch_exports/test_run_updates.py
@@ -18,66 +18,68 @@ from posthog.temporal.workflows.base import (
 from posthog.test.base import TransactionTestCase
 
 
-@pytest.fixture
-def organization():
-    """Fixture providing an Organization for testing."""
-    org = Organization.objects.create(name="test-org")
-    org.save()
-
-    yield org
-
-    org.delete()
-
-
-@pytest.fixture
-def team(organization):
-    """Fixture providing a Team for testing."""
-    team = Team.objects.create(organization=organization)
-    team.save()
-
-    yield team
-
-    team.delete()
-
-
-@pytest.fixture
-def destination(team):
-    """Fixture providing an BatchExportDestination for testing."""
-    dest = BatchExportDestination.objects.create(
-        type="S3",
-        config={
-            "bucket_name": "bucket",
-            "region": "us-east-1",
-            "prefix": "posthog-events/",
-            "batch_window_size": 3600,
-            "aws_access_key_id": "key_id",
-            "aws_secret_access_key": "secret",
-        },
-    )
-    dest.save()
-
-    yield dest
-
-    dest.delete()
-
-
-@pytest.fixture
-def batch_export(destination, team):
-    """A test BatchExport."""
-    batch_export = BatchExport.objects.create(name="test export", team=team, destination=destination, interval="hour")
-
-    batch_export.save()
-
-    yield batch_export
-
-    batch_export.delete()
-
-
 class RunUpdatesTest(TransactionTestCase):
     available_apps = ["posthog"]
 
+    @pytest.fixture(autouse=True)
+    def activity_environment_fixture(self, activity_environment):
+        """Fixture providing an Organization for testing."""
+        self.activity_environment = activity_environment
+
+    @pytest.fixture(autouse=True)
+    def organization(self):
+        """Fixture providing an Organization for testing."""
+        self.org = Organization.objects.create(name="test-org")
+        self.org.save()
+
+        yield
+
+        self.org.delete()
+
+    @pytest.fixture(autouse=True)
+    def team_fixture(self, organization):
+        """Fixture providing a Team for testing."""
+        self.team = Team.objects.create(organization=self.org)
+        self.team.save()
+
+        yield
+
+        self.team.delete()
+
+    @pytest.fixture(autouse=True)
+    def destination(self, team_fixture):
+        """Fixture providing an BatchExportDestination for testing."""
+        self.dest = BatchExportDestination.objects.create(
+            type="S3",
+            config={
+                "bucket_name": "bucket",
+                "region": "us-east-1",
+                "prefix": "posthog-events/",
+                "batch_window_size": 3600,
+                "aws_access_key_id": "key_id",
+                "aws_secret_access_key": "secret",
+            },
+        )
+        self.dest.save()
+
+        yield
+
+        self.dest.delete()
+
+    @pytest.fixture(autouse=True)
+    def batch_export(self, team_fixture, destination):
+        """A test BatchExport."""
+        self.batch_export = BatchExport.objects.create(
+            name="test export", team=self.team, destination=self.dest, interval="hour"
+        )
+        self.batch_export.save()
+
+        yield
+
+        self.batch_export.delete()
+
     @pytest.mark.asyncio
-    async def test_create_export_run(activity_environment, team, batch_export):
+    async def test_create_export_run(self):
         """Test the create_export_run activity.
 
         We check if an BatchExportRun is created after the activity runs.
@@ -86,13 +88,13 @@ class RunUpdatesTest(TransactionTestCase):
         end = dt.datetime(2023, 4, 25, tzinfo=dt.timezone.utc)
 
         inputs = CreateBatchExportRunInputs(
-            team_id=team.id,
-            batch_export_id=str(batch_export.id),
+            team_id=self.team.id,
+            batch_export_id=str(self.batch_export.id),
             data_interval_start=start.isoformat(),
             data_interval_end=end.isoformat(),
         )
 
-        run_id = await activity_environment.run(create_export_run, inputs)
+        run_id = await self.activity_environment.run(create_export_run, inputs)
 
         runs = BatchExportRun.objects.filter(id=run_id)
         assert await sync_to_async(runs.exists)()  # type:ignore
@@ -102,19 +104,19 @@ class RunUpdatesTest(TransactionTestCase):
         assert run.data_interval_end == end
 
     @pytest.mark.asyncio
-    async def test_update_export_run_status(activity_environment, team, batch_export):
+    async def test_update_export_run_status(self):
         """Test the export_run_status activity."""
         start = dt.datetime(2023, 4, 24, tzinfo=dt.timezone.utc)
         end = dt.datetime(2023, 4, 25, tzinfo=dt.timezone.utc)
 
         inputs = CreateBatchExportRunInputs(
-            team_id=team.id,
-            batch_export_id=str(batch_export.id),
+            team_id=self.team.id,
+            batch_export_id=str(self.batch_export.id),
             data_interval_start=start.isoformat(),
             data_interval_end=end.isoformat(),
         )
 
-        run_id = await activity_environment.run(create_export_run, inputs)
+        run_id = await self.activity_environment.run(create_export_run, inputs)
 
         runs = BatchExportRun.objects.filter(id=run_id)
         run = await sync_to_async(runs.first)()  # type:ignore
@@ -124,7 +126,7 @@ class RunUpdatesTest(TransactionTestCase):
             id=str(run_id),
             status="Completed",
         )
-        await activity_environment.run(update_export_run_status, update_inputs)
+        await self.activity_environment.run(update_export_run_status, update_inputs)
 
         runs = BatchExportRun.objects.filter(id=run_id)
         run = await sync_to_async(runs.first)()  # type:ignore
